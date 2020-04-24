276cec369fb498bd416d271cf9e1dd62e754c2f6
PabloGarciaGrossi <pabgar20@ucm.es>
2020-04-24 20:44:50 +0200

    Primer Ejercicio

---
+++ rvr_repo/practica2.1/ejercicio1/ejercicio1.cc
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+  struct addrinfo hints;
+  struct addrinfo * res;
+
+  memset(&hints, 0, sizeof(struct addrinfo));
+
+  //AF_UNSPEC -> OPCIÓN QUE PERMITE OBTENER TODAS LAS FAMILIAS
+  hints.ai_family = AF_UNSPEC;
+
+  int rc = getaddrinfo(argv[1], nullptr , &hints, &res);
+
+  if ( rc != 0 )
+  {
+    std::cerr << "getaddrinfo: " << gai_strerror(rc) << std::endl;
+    return -1;
+  }
+  socklen_t client_len = sizeof(struct sockaddr);
+  char host[NI_MAXHOST];
+
+  while (res != NULL)
+  {
+    getnameinfo(res->ai_addr, res->ai_addrlen, host, NI_MAXHOST, NULL, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
+
+    std::cout << "IP: " << host << " FAMILY: " << res->ai_family << " SOCKTYPE: " << res->ai_socktype << std::endl;
+    res = res->ai_next;
+  }
+  freeaddrinfo(res);
+  return 0;
+}
